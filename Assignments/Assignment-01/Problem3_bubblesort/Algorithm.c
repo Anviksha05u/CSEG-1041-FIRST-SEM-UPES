5521c3d68943620274481f14cc251d5e172fec4e
Anviksha <Anviksha.26976@stu.upes.ac.in>
2025-08-19 01:54:18 +0530

    Algorithm.c

--- Assignments/Assignment-01/Problem3_bubblesort/Algorithm.c
+++ Assignments/Assignment-01/Problem3_bubblesort/Algorithm.c
@@ -1,5 +1,5 @@
 START
-INPUT N (size of array) and Array Elements
+INPUT N (size of array) and arr (array elements)
 RUN a Nested Loop from i=0 to N-1 and inside it from j=0 to N-i-1
 Inside it CHECK if arr[j] > arr[j+1]
 IF yes, then SWAP them.
