6dd3d4b5dc9172554576803cb5d42e5db73cb8b6
Anviksha <Anviksha.26976@stu.upes.ac.in>
2025-09-07 18:26:43 +0530

    Update Q1.c

--- Experiment 3.1/Q1.c
+++ Experiment 3.1/Q1.c
@@ -1,8 +1,9 @@
 //To check if the triangle is valid or not. If it is then check if the triangle is isosceles, scalene or equilateral.
 #include <stdio.h>
+#include <math.h>
 int main()
 {
-  int a,b,c;
+  int a,b,c,h,b1,b2;
   printf("Enter three sides of the triangle: ");
   scanf("%d %d %d", &a, &b, &c);
   if (a + b > c && b + c > a && c + a > b) 
@@ -20,7 +21,29 @@ int main()
    {
     printf("Scalene triangle\n");
    }
-  } 
+   if (a>b && a>c)
+   {
+    h=a;
+    b1=b;
+    b2=c;
+   }
+   else if (b>c && b>a)
+   {
+    h=b;
+    b1=c;
+    b2=a;
+   }
+   else if (c>a && c>b)
+   {
+    h=c;
+    b1=a;
+    b2=b;
+   }
+   else if (h*h == (b1*b1 + b2*b2))
+   {
+    printf ("IT IS A RIGHT ANGLED TRIANGLE\n");
+   }
+  }
   else 
   {
    printf("Not a triangle\n");
