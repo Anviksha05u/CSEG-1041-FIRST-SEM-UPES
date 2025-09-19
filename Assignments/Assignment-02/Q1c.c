c28808ca7bd4dda5c004c63d34e3a6920248f50f
Anviksha <Anviksha.26976@stu.upes.ac.in>
2025-09-19 14:54:10 +0530

    push

--- Assignments/Assignment-02/Q1c.c
+++ Assignments/Assignment-02/Q1c.c
@@ -1,1 +1,33 @@
 //Write a program to input n integers into an array and Count how many numbers are even and how many are odd using loops. 
+
+#include <stdio.h>
+int main()
+{
+    int n, i;//Variable declaration for number of elements and loop counter
+    int evenCount = 0, oddCount = 0; //To count even and odd numbers
+    //Input the number of elements in the array
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+    int arr[n]; // Declare an array of size n
+    //Take input of the elements and store it in the array
+    printf("Enter elements: ");
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    //Loop to check each number and count even and odd numbers
+    for (i = 0; i < n; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            evenCount++;
+        }
+        else
+        {
+            oddCount++;
+        }
+    }
+    //Display the results
+    printf("Total even numbers: %d\n", evenCount);
+    printf("Total odd numbers: %d\n", oddCount);
+}
---
+++ Assignments/Assignment-02/outputQ1c.c
@@ -0,0 +1,9 @@
+Enter the number of elements: 3
+Enter elements: 1 9 6
+Total even numbers: 1
+Total odd numbers: 2
+
+Enter the number of elements: 8
+Enter elements: 1 2 3 4 5 6 7 8
+Total even numbers: 4
+Total odd numbers: 4
