9ba46c5477420feacbd2db294301b95f2cf549b6
Anviksha <Anviksha.26976@stu.upes.ac.in>
2025-09-19 14:36:03 +0530

    push

--- Assignments/Assignment-02/Q1b.c
+++ Assignments/Assignment-02/Q1b.c
@@ -1,1 +1,25 @@
 //Write a program to input n integers into an array and Calculate the average of all numbers using loops.
+
+#include <stdio.h>
+int main()
+{
+    int n, i; //Variable declaration for number of elements and loop counter
+    int sum = 0; //To store the sum of all numbers
+    float average; //To store the average
+    //Input the number of elements in the array
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+    int arr[n]; // Declare an array of size n
+    //Take input of the elements and store it in the array
+    printf("Enter elements: ");
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+        sum += arr[i]; // Adding each number to sum
+    }
+    //Calculate average
+    average = (float)sum / n; //Explicit conversion to float for decimal results
+    //Display the result
+    printf("The average of the numbers is: %.2f", average);
+    return 0;
+}
---
+++ Assignments/Assignment-02/outputQ1b.c
@@ -0,0 +1,7 @@
+Enter the number of elements: 4
+Enter elements: 3 6 7 4
+The average of the numbers is: 5.00
+
+Enter the number of elements: 6
+Enter elements: 4 2 1 0 7 5
+The average of the numbers is: 3.17
