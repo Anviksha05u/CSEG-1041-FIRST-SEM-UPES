2a0ba7b04c24bae83b3bc6e0736718d610ccc72d
Anviksha <Anviksha.26976@stu.upes.ac.in>
2025-09-19 15:59:16 +0530

    push

--- Assignments/Assignment-02/Q3.c
+++ Assignments/Assignment-02/Q3.c
@@ -1,1 +1,66 @@
 // Write a program to input 10 integers (range 0–9). Store them in an array. Then, using a switch-case, print the frequency of each digit.
+
+#include <stdio.h>
+int main()
+{
+    int arr[10]; //Array to store 10 integers (0-9)
+    int freq[10] = {0}; //Frequency array initialized to 0
+    int i; //Variable declaration for loop counter
+    //Take input of the elements and store it in the array
+    printf("Enter elements (0-9): ");
+    for (i = 0; i < 10; i++)
+    {
+        scanf("%d", &arr[i]);
+        if (arr[i] < 0 || arr[i] > 9) 
+        {
+            printf("Invalid input!\n");
+            i--;
+        }
+    }
+    //Count frequency
+    for (i = 0; i < 10; i++)
+    {
+        freq[arr[i]]++;
+    }
+    //Print frequency using switch-case
+    printf("Frequency of each digit:\n");
+    for (i = 0; i < 10; i++)
+    {
+        switch (i)
+        {
+            case 0: 
+            printf("Digit 0 occurs %d times\n", freq[i]);
+            break;
+            case 1: 
+            printf("Digit 1 occurs %d times\n", freq[i]);
+            break;
+            case 2: 
+            printf("Digit 2 occurs %d times\n", freq[i]);
+            break;
+            case 3: 
+            printf("Digit 3 occurs %d times\n", freq[i]);
+            break;
+            case 4: 
+            printf("Digit 4 occurs %d times\n", freq[i]);
+            break;
+            case 5: 
+            printf("Digit 5 occurs %d times\n", freq[i]);
+            break;
+            case 6: 
+            printf("Digit 6 occurs %d times\n", freq[i]);
+            break;
+            case 7: 
+            printf("Digit 7 occurs %d times\n", freq[i]);
+            break;
+            case 8: 
+            printf("Digit 8 occurs %d times\n", freq[i]);
+            break;
+            case 9: 
+            printf("Digit 9 occurs %d times\n", freq[i]);
+            break;
+            default: 
+            printf("Invalid digit!\n");
+            break;
+        }
+    }
+}
---
+++ Assignments/Assignment-02/outputQ3.c
@@ -0,0 +1,29 @@
+Enter elements (0-9): 1 2 3 4 5 6 7 8 9 0
+Frequency of each digit:
+Digit 0 occurs 1 times
+Digit 1 occurs 1 times
+Digit 2 occurs 1 times
+Digit 3 occurs 1 times
+Digit 4 occurs 1 times
+Digit 5 occurs 1 times
+Digit 6 occurs 1 times
+Digit 7 occurs 1 times
+Digit 8 occurs 1 times
+Digit 9 occurs 1 times
+
+Enter elements (0-9): 1 2 3 4 5 70 12 35 9 0
+Invalid input!
+Invalid input!
+Invalid input!
+1 2 3
+Frequency of each digit:
+Digit 0 occurs 1 times
+Digit 1 occurs 2 times
+Digit 2 occurs 2 times
+Digit 3 occurs 2 times
+Digit 4 occurs 1 times
+Digit 5 occurs 1 times
+Digit 6 occurs 0 times
+Digit 7 occurs 0 times
+Digit 8 occurs 0 times
+Digit 9 occurs 1 times
