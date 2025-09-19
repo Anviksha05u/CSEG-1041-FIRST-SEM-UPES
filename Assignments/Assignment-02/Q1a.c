2d31a80075c2743965b2d661cbc83031b8c3b4a6
Anviksha <Anviksha.26976@stu.upes.ac.in>
2025-09-19 14:15:06 +0530

    push

--- Assignments/Assignment-02/Q1a.c
+++ Assignments/Assignment-02/Q1a.c
@@ -1,1 +1,34 @@
 // Write a program to input n integers into an array and Find the largest and smallest number using loops. 
+
+#include <stdio.h>
+int main()
+{
+    int n, i; //Variable declaration for number of elements and loop counter
+    //Input the number of elements in the array
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+    int arr[n]; // Declare an array of size n
+    //Take input of the elements and store it in the array
+    printf("Enter elements: ");
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    int l = arr[0], s = arr[0]; //Initialize largest and smallest number in the array with the first element of the array
+    //Loop to find the largest and smallest number in the array
+    for (i = 1; i < n; i++)
+    {
+        if (arr[i] > l)
+        {
+            l = arr[i];
+        }
+        if (arr[i] < s)
+        {
+            s = arr[i];
+        }
+    }
+    //Display the results
+    printf("The largest number is: %d\n", l);
+    printf("The smallest number is: %d\n", s);
+    return 0;
+}
---
+++ Assignments/Assignment-02/outputQ1a.c
@@ -0,0 +1,9 @@
+Enter the number of elements: 5
+Enter elements: 2 4 6 8 10
+The largest number is: 10
+The smallest number is: 2
+
+Enter the number of elements: 4
+Enter elements: 2 6 3 1
+The largest number is: 6
+The smallest number is: 1
