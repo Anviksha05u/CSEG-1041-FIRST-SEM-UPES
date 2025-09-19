919e64bd47e92d9fcc51812b9aef968ef043f8fb
Anviksha <Anviksha.26976@stu.upes.ac.in>
2025-09-19 18:55:05 +0530

    Q7.c

---
+++ Assignments/Assignment-02/Q7.c
@@ -0,0 +1,8 @@
+/*
+Assume a class of 5 students, each having marks in 3 subjects. Store the marks in a 2D array where each row represents a student and each column represents a subject.
+Perform the following:
+ (a) Calculate the total and average marks of each student.
+ (b) Find the highest scorer (student with maximum total marks).
+ (c) Find the subject in which the class performed worst (lowest average marks).
+ (d) Use sizeof() operator to display the total memory consumed by the array.
+*/
