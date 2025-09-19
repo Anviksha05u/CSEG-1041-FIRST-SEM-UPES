41c9c0fdad54ee366cc7241898088473ce9a60e7
Anviksha <Anviksha.26976@stu.upes.ac.in>
2025-09-19 15:14:02 +0530

    push

--- Assignments/Assignment-02/Q2.c
+++ Assignments/Assignment-02/Q2.c
@@ -1,1 +1,34 @@
 // Write a program to input n integers in an array. Ask the user for a number and check if it exists in the array using the ternary operator. Print "Found" or "Not Found".
+
+#include <stdio.h>
+int main()
+{
+    int n,i; //Variable declaration for number of elements and loop counter
+    int ns; //Number to be searched
+    int found = 0; //Flag variable
+    //Input the number of elements in the array
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+    int arr[n]; // Declare an array of size n
+    //Take input of the elements and store it in the array
+    printf("Enter elements: ");
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    //Input the number to be searched
+    printf("Enter the number to search: ");
+    scanf("%d", &ns);
+    //Loop to search for the number in array
+    for (i = 0; i < n; i++)
+    {
+        if (arr[i] == ns)
+        {
+            found = 1; //Set flag to 1 if the element is found
+            break; //exit loop once found
+        }
+    }
+    //Display the results
+    printf("%s", (found == 1) ? "Found" : "Not Found");
+    return 0;
+}
---
+++ Assignments/Assignment-02/outputQ2.c
@@ -0,0 +1,9 @@
+Enter the number of elements: 5
+Enter elements: 10 20 30 40 50
+Enter the number to search: 40
+Found
+
+Enter the number of elements: 6
+Enter elements: 1 4 3 2 7 9
+Enter the number to search: 6
+Not Found
