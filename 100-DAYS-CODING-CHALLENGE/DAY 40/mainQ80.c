cf0e4d24950e9e037db7afdc1e374da8fd8a6a1c
Anviksha <Anviksha.26976@stu.upes.ac.in>
2025-09-30 04:14:59 +0530

    Added C program and sample output

--- 100-DAYS-CODING-CHALLENGE/DAY 40/mainQ80.c
+++ 100-DAYS-CODING-CHALLENGE/DAY 40/mainQ80.c
@@ -3,63 +3,76 @@
 #include <stdio.h>
 int main()
 {
-    int r, c, i, j;
+    int r1, c1, r2, c2, i, j, k;
     printf("Enter number of rows of first matrix: ");
-    scanf("%d", &r);
+    scanf("%d", &r1);
     printf("Enter number of columns of first matrix: ");
-    scanf("%d", &c);
-    int matrix1[r][c]; 
-    printf("Enter elements of the first matrix: \n");
-    for (i = 0; i < r; i++)
+    scanf("%d", &c1);
+    printf("Enter number of rows of second matrix: ");
+    scanf("%d", &r2);
+    printf("Enter number of columns of second matrix: ");
+    scanf("%d", &c2);
+    if (c1 != r2)
     {
-        for (j = 0; j < c; j++)
+        printf("Matrix Multiplication not possible");
+        return 0;
+    }
+    int matrix1[r1][c1], matrix2[r2][c2], prod[r1][c2]; 
+    printf("Enter elements of the first matrix: \n");
+    for (i = 0; i < r1; i++)
+    {
+        for (j = 0; j < c1; j++)
         {
             scanf("%d", &matrix1[i][j]);
         }
     }
     printf("\nFirst Matrix: \n");
-    for (i = 0; i < r; i++) 
+    for (i = 0; i < r1; i++) 
     {
-        for (j = 0; j < c; j++) 
+        for (j = 0; j < c1; j++) 
         {
             printf("%d ", matrix1[i][j]);
         }
         printf("\n");
     }
-    printf("Enter number of rows of second matrix: ");
-    scanf("%d", &r);
-    printf("Enter number of columns of second matrix: ");
-    scanf("%d", &c);
     printf("Enter elements of the second matrix: \n");
-    int matrix2[r][c];
-    for (i = 0; i < r; i++)
+    for (i = 0; i < r2; i++)
     {
-        for (j = 0; j < c; j++)
+        for (j = 0; j < c2; j++)
         {
             scanf("%d", &matrix2[i][j]);
         }
     }
     printf("\nSecond Matrix: \n");
-    for (i = 0; i < r; i++) 
+    for (i = 0; i < r2; i++) 
     {
-        for (j = 0; j < c; j++) 
+        for (j = 0; j < c2; j++) 
         {
             printf("%d ", matrix2[i][j]);
         }
         printf("\n");
     }
-    int prod[r][c];
-    for (i = 0; i < r; i++)
+    for (i = 0; i < r1; i++)
     {
-        for (j = 0; j < c; j++)
+        for (j = 0; j < c2; j++)
         {
-            prod[i][j] = matrix1[i][j] * matrix2[i][j];
+            prod[i][j] = 0;
+        }
+    }
+    for (i = 0; i < r1; i++)
+    {
+        for (j = 0; j < c2; j++)
+        {
+            for (k = 0; k < c1; k++)
+            {
+            prod[i][j] += matrix1[i][k] * matrix2[k][j];
+            }
         }
     }
     printf("\nMatrix Multiplication: \n");
-    for (i = 0; i < r; i++)
+    for (i = 0; i < r1; i++)
     {
-        for (j = 0; j < c; j++)
+        for (j = 0; j < c2; j++)
         {
             printf("%d ", prod[i][j]);
         }
