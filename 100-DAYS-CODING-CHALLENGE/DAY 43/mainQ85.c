f3241d63b2ba352465dec4cbd250da6b83878e19
Anviksha <Anviksha.26976@stu.upes.ac.in>
2025-10-09 11:14:30 +0530

    mainQ85.c

---
+++ 100-DAYS-CODING-CHALLENGE/DAY 43/mainQ85.c
@@ -0,0 +1,25 @@
+//Reverse a string.
+
+#include <stdio.h>
+#include <string.h>
+// Function to reverse the string
+void reverse(char str[])
+{
+    int len=strlen(str);
+    // Swap characters from start and end moving toward the center
+    for(int i=0;i<len/2;i++)
+    {
+        char temp=str[i];
+        str[i]=str[len-1-i];
+        str[len-1-i]=temp;
+    }
+}
+int main() 
+{
+    char str[100];
+    printf("enter the string: ");
+    scanf("%s",str);
+    reverse(str);// Call the reverse function
+    printf("The reversed string %s\n",str);
+    return 0;
+}
