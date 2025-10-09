86d2a95524fcc770d225248a04e4a0e9e0b49b79
Anviksha <Anviksha.26976@stu.upes.ac.in>
2025-10-09 21:34:15 +0530

    Added C program and sample output

--- 100-DAYS-CODING-CHALLENGE/DAY 49/mainQ98.c
+++ 100-DAYS-CODING-CHALLENGE/DAY 49/mainQ98.c
@@ -1,13 +1,12 @@
-//Print the initials of a name.
+//Print initials of a name with the surname displayed in full.
 
 #include <stdio.h>
 int main() 
 {
     char str[1000];
-    int i = 0;
+    int i = 0, lastSpace = -1;
     printf("Enter a name: ");
     fgets(str, sizeof(str), stdin);
-    printf("Initials: ");
     if (str[0] != ' ') 
     {
         printf("%c.", str[0]);
@@ -16,9 +15,14 @@ int main()
     {
         if (str[i] == ' ' && str[i + 1] != ' ' && str[i + 1] != '\0') 
         {
+            lastSpace = i;
             printf("%c.", str[i + 1]);
         }
         i++;
+    }
+    if (lastSpace != -1) 
+    {
+        printf(" %s", &str[lastSpace + 1]);
     }
     return 0;
 }
