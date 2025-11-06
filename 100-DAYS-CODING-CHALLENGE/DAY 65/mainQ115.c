f48c131167b4839c0b54bcbd8609501c804b830d
Anviksha <Anviksha.26976@stu.upes.ac.in>
2025-11-07 01:18:32 +0530

    mainQ115.c

---
+++ 100-DAYS-CODING-CHALLENGE/DAY 65/mainQ115.c
@@ -0,0 +1,2 @@
+//Write a program to take two strings s and t as inputs (assume all characters are lowercase). The task is to determine if s and t are valid anagrams, meaning they contain the same characters with the same frequencies. Print "Anagram" if they are, otherwise "Not Anagram".
+
