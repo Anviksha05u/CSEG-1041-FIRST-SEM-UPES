ad7f9625552e902e99ab82fd5e7c3a7f09b01200
Anviksha <Anviksha.26976@stu.upes.ac.in>
2025-11-17 14:35:29 +0530

    Added sample output

--- 100-DAYS-CODING-CHALLENGE/DAY 82/mainQ132.c
+++ 100-DAYS-CODING-CHALLENGE/DAY 82/mainQ132.c
@@ -1,2 +1,15 @@
 //Define an enum for traffic lights (RED, YELLOW, GREEN) and print 'Stop', 'Wait', or 'Go' based on its value.
 
+#include <stdio.h>
+int main() 
+{
+    enum Month {JANUARY, FEBRUARY, MARCH, APRIL, MAY, JUNE, JULY, AUGUST, SEPTEMBER, OCTOBER, NOVEMBER, DECEMBER};
+    const char *monthNames[] = {"JANUARY", "FEBRUARY", "MARCH", "APRIL", "MAY", "JUNE", "JULY", "AUGUST", "SEPTEMBER", "OCTOBER", "NOVEMBER", "DECEMBER"};
+    int daysInMonth[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    for (int i = JANUARY; i <= DECEMBER; i++) 
+    {
+        printf("%s = %d days\n", monthNames[i], daysInMonth[i]);
+    }
+
+    return 0;
+}
