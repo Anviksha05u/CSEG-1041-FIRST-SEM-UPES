2bc6db073853e671e57380eeee071fc6ba319d63
Anviksha <Anviksha.26976@stu.upes.ac.in>
2025-11-17 15:00:09 +0530

    Added sample output

--- 100-DAYS-CODING-CHALLENGE/DAY 82/mainQ132.c
+++ 100-DAYS-CODING-CHALLENGE/DAY 82/mainQ132.c
@@ -1,15 +1,42 @@
 //Define an enum for traffic lights (RED, YELLOW, GREEN) and print 'Stop', 'Wait', or 'Go' based on its value.
 
 #include <stdio.h>
+#include <string.h>
 int main() 
 {
-    enum Month {JANUARY, FEBRUARY, MARCH, APRIL, MAY, JUNE, JULY, AUGUST, SEPTEMBER, OCTOBER, NOVEMBER, DECEMBER};
-    const char *monthNames[] = {"JANUARY", "FEBRUARY", "MARCH", "APRIL", "MAY", "JUNE", "JULY", "AUGUST", "SEPTEMBER", "OCTOBER", "NOVEMBER", "DECEMBER"};
-    int daysInMonth[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-    for (int i = JANUARY; i <= DECEMBER; i++) 
+    enum TrafficLight { RED, YELLOW, GREEN };
+    enum TrafficLight current;
+    char input[10];
+    printf("Enter traffic light color: ");
+    scanf("%s", input);
+    if (strcmp(input, "RED") == 0) 
     {
-        printf("%s = %d days\n", monthNames[i], daysInMonth[i]);
+        current = RED;
+    } 
+    else if (strcmp(input, "YELLOW") == 0) 
+    {
+        current = YELLOW;
+    } 
+    else if (strcmp(input, "GREEN") == 0) 
+    {
+        current = GREEN;
+    } 
+    else 
+    {
+        printf("Invalid input. Please enter RED, YELLOW, or GREEN.\n");
+        return 1;
     }
-
+    switch (current) 
+    {
+        case RED:
+            printf("Stop\n");
+            break;
+        case YELLOW:
+            printf("Wait\n");
+            break;
+        case GREEN:
+            printf("Go\n");
+            break;
+    }
     return 0;
 }
