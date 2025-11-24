984e845032465df418e906c8479df70ac01ccc60
Anviksha <Anviksha.26976@stu.upes.ac.in>
2025-11-24 19:35:46 +0530

    push

--- EXPERIMENT 10/Q1.c
+++ EXPERIMENT 10/Q1.c
@@ -1,19 +1,28 @@
+//Write a program to create a simple linked list in C using pointer and structure. 
 #include<stdio.h>
 #include<stdlib.h>
-Struct Node
+struct Node
 {
     int data;
-    Struct Node *next;
+    struct Node *next;
 };
 int main()
 {
-    Struct Node *head = NULL, *second = NULL, *end = NULL;
-    head = (Struct Node*) malloc (sizeof(Struct Node));
-    second = (Struct Node*) malloc (sizeof(Struct Node));
-    end = (Struct Node*) malloc (sizeof(Struct Node));
+    struct Node *head = NULL, *second = NULL, *end = NULL;
+    head = (struct Node*) malloc (sizeof(struct Node));
+    second = (struct Node*) malloc (sizeof(struct Node));
+    end = (struct Node*) malloc (sizeof(struct Node));
     head -> data = 50;
     head -> next = second;
     second -> data = 100;
     second -> next = end;
-    
+    end -> data = 150;
+    end -> next = NULL;
+    struct Node *temp = head;
+    while(temp != NULL)
+    {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+    return 0;
 }
